79d5a2b7c4c8da6e4bbd01e03e2a0f9f1aba5008
Yauheni Tsiarokhin <yauheni@tsiarokhin.com>
2021-02-06 21:51:49 +0300

    move commands' parsing to companion ibjects

--- src/main/scala/control_structures/Command.scala
+++ src/main/scala/control_structures/Command.scala
@@ -20,11 +20,11 @@ object Command {
     val tokens = raw.trim().split(' ').filter(_ != "").toList
 
     tokens match {
-      case Command.AVERAGE :: args => parseCommandAverage(args)
-      case Command.DIVIDE :: args  => parseCommandDivide(args)
-      case Command.MAX :: args     => parseCommandMax(args)
-      case Command.MIN :: args     => parseCommandMin(args)
-      case Command.SUM :: args     => parseCommandSum(args)
+      case Command.AVERAGE :: args => Command.Average.parse(args)
+      case Command.DIVIDE :: args  => Command.Divide.parse(args)
+      case Command.MAX :: args     => Command.Max.parse(args)
+      case Command.MIN :: args     => Command.Min.parse(args)
+      case Command.SUM :: args     => Command.Sum.parse(args)
       case command :: _            => Left(ErrorMessage(s"unknown command: $command"))
       case Nil                     => Left(ErrorMessage("empty input"))
     }
@@ -47,55 +47,6 @@ object Command {
       .map(_.reverse)
   }
 
-  def parseCommandDivide(
-      args: List[String]
-  ): Either[ErrorMessage, Command.Divide] = {
-    parseCommandArgs(args).flatMap(args =>
-      args match {
-        case arg1 :: arg2 :: Nil => Right(Command.Divide(arg1, arg2))
-        case _                   => Left(ErrorMessage("command DIVIDE accepts exactly 2 args"))
-      }
-    )
-  }
-
-  def parseCommandAverage(
-      args: List[String]
-  ): Either[ErrorMessage, Command.Average] = {
-    parseCommandArgs(args).flatMap(args =>
-      args match {
-        case _ :: _ => Right(Command.Average(args))
-        case _      => Left(ErrorMessage("command AVERAGE accepts at least 1 arg"))
-      }
-    )
-  }
-
-  def parseCommandSum(args: List[String]): Either[ErrorMessage, Command.Sum] = {
-    parseCommandArgs(args).flatMap(args =>
-      args match {
-        case _ :: _ => Right(Command.Sum(args))
-        case _      => Left(ErrorMessage("command SUM accepts at least 1 arg"))
-      }
-    )
-  }
-
-  def parseCommandMin(args: List[String]): Either[ErrorMessage, Command.Min] = {
-    parseCommandArgs(args).flatMap(args =>
-      args match {
-        case _ :: _ => Right(Command.Min(args))
-        case _      => Left(ErrorMessage("command MIN accepts at least 1 arg"))
-      }
-    )
-  }
-
-  def parseCommandMax(args: List[String]): Either[ErrorMessage, Command.Max] = {
-    parseCommandArgs(args).flatMap(args =>
-      args match {
-        case _ :: _ => Right(Command.Max(args))
-        case _      => Left(ErrorMessage("command MAX accepts at least 1 arg"))
-      }
-    )
-  }
-
   final case class Divide(dividend: Arg, divisor: Arg) extends Command {
     override def calculateResult(): Either[ErrorMessage, Result] =
       divisor match {
@@ -106,6 +57,16 @@ object Command {
     override def renderResult(result: Result): String =
       s"$dividend divided by $divisor is $result"
   }
+  final object Divide {
+    def parse(args: List[String]): Either[ErrorMessage, Command.Divide] = {
+      parseCommandArgs(args).flatMap(args =>
+        args match {
+          case arg1 :: arg2 :: Nil => Right(Command.Divide(arg1, arg2))
+          case _                   => Left(ErrorMessage("command DIVIDE accepts exactly 2 args"))
+        }
+      )
+    }
+  }
 
   final case class Sum(args: Args) extends Command {
     override def calculateResult(): Either[ErrorMessage, Result] =
@@ -113,6 +74,16 @@ object Command {
 
     override def renderResult(result: Result): String =
       s"the sum of ${args.mkString(" ")} is $result"
+  }
+  final object Sum {
+    def parse(args: List[String]): Either[ErrorMessage, Command.Sum] = {
+      parseCommandArgs(args).flatMap(args =>
+        args match {
+          case _ :: _ => Right(Command.Sum(args))
+          case _      => Left(ErrorMessage("command SUM accepts at least 1 arg"))
+        }
+      )
+    }
   }
 
   final case class Average(args: Args) extends Command {
@@ -122,6 +93,16 @@ object Command {
     override def renderResult(result: Result): String =
       s"the average of ${args.mkString(" ")} is $result"
   }
+  final object Average {
+    def parse(args: List[String]): Either[ErrorMessage, Command.Average] = {
+      parseCommandArgs(args).flatMap(args =>
+        args match {
+          case _ :: _ => Right(Command.Average(args))
+          case _      => Left(ErrorMessage("command AVERAGE accepts at least 1 arg"))
+        }
+      )
+    }
+  }
 
   final case class Min(args: Args) extends Command {
     override def calculateResult(): Either[ErrorMessage, Result] =
@@ -130,6 +111,16 @@ object Command {
     override def renderResult(result: Result): String =
       s"the minimum of ${args.mkString(" ")} is $result"
   }
+  final object Min {
+    def parse(args: List[String]): Either[ErrorMessage, Command.Min] = {
+      parseCommandArgs(args).flatMap(args =>
+        args match {
+          case _ :: _ => Right(Command.Min(args))
+          case _      => Left(ErrorMessage("command MIN accepts at least 1 arg"))
+        }
+      )
+    }
+  }
 
   final case class Max(args: Args) extends Command {
     override def calculateResult(): Either[ErrorMessage, Result] =
@@ -137,5 +128,15 @@ object Command {
 
     override def renderResult(result: Result): String =
       s"the maximum of ${args.mkString(" ")} is $result"
+  }
+  final object Max {
+    def parse(args: List[String]): Either[ErrorMessage, Command.Max] = {
+      parseCommandArgs(args).flatMap(args =>
+        args match {
+          case _ :: _ => Right(Command.Max(args))
+          case _      => Left(ErrorMessage("command MAX accepts at least 1 arg"))
+        }
+      )
+    }
   }
 }
